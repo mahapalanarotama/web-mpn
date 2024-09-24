2117dc5ca3300e2105617e23051014bb144a53a3
Fairus-24 <mufaza2408@gmail.com>
2024-09-24 20:59:37 +0700

    new

---
+++ script.js
@@ -0,0 +1,18 @@
+// Script sederhana untuk efek smooth scroll di navbar
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
+// Function to toggle the visibility of the menu in mobile view
+function toggleMenu() {
+    var navLinks = document.getElementById("nav-links");
+    if (navLinks.style.display === "flex") {
+        navLinks.style.display = "none";
+    } else {
+        navLinks.style.display = "flex";
+    }
+}
